7d46d782acc9001dfd014d1781fb579f63433d10
jodaruve <jose.rueda.2019@upb.edu.co>
2019-02-22 20:10:26 -0500

    mi primera modificacion

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ producto=a*c
 d_a=a*2
 print("  El producto de", a, "y", c, "es:", producto)
 print("  El doble del número", a, "es:", d_a)
+b=int(input("-Por favor ingrese un valor para saber cual es su cuadrado "))
+cuadrado=b**2
+print("  El cuadrado de", b, "es:", cuadrado)
+d=int(input("-Por favor ingrese un valor para conocer cual es su raiz cuadrada "))
+raiz=d**(1/2)
+print("  La raiz cuadrada de", d, "es:", raiz)
