10b42e40a811c6fc4cff93a92e9e8deadceeddf1
jodaruve <jose.rueda.2019@upb.edu.co>
2019-02-22 20:05:00 -0500

    mi primer commit

---
+++ main.py
@@ -0,0 +1,6 @@
+a=int(input("-Por favor ingrese un valor "))
+c=int(input("-Por favor ingrese otro valor "))
+producto=a*c
+d_a=a*2
+print("  El producto de", a, "y", c, "es:", producto)
+print("  El doble del número", a, "es:", d_a)
