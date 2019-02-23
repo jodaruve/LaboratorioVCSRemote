68616f6efe350f691c599acff580582f05c1e67f
jodaruve <jose.rueda.2019@upb.edu.co>
2019-02-23 11:15:13 -0500

    solucion

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-a=int(input("-Por favor ingrese un valor "))
-c=int(input("-Por favor ingrese otro valor "))
-producto=a*c
-d_a=a*2
-print("  El producto de", a, "y", c, "es:", producto)
-print("  El doble del número", a, "es:", d_a)
-b=int(input("-Por favor ingrese un valor para saber cual es su cuadrado "))
-cuadrado=b**2
-print("  El cuadrado de", b, "es:", cuadrado)
-d=int(input("-Por favor ingrese un valor para conocer cual es su raiz cuadrada "))
-raiz=d**(1/2)
-print("  La raiz cuadrada de", d, "es:", raiz)
+print ("x=-b+-(b**2-4*a*c)**(1/2)/2*a")
+a=float(input("Por favor ingrese el valor para reemplazar 'a' "))
+b=float(input("Por favor ingrese el valor para reemplazar 'b' "))
+c=float(input("Por favor ingrese el valor para reemplazar 'c' "))
+d=(((b**2)-(4*a*c))**(1/2))
+x1=(((-1*b)-(d**(1/2))/2*a))
+x2=(((-1*b)+(d**(1/2))/2*a))
+if d>0:
+	print("la seccion negativa es: ", x1, "la parte positiva es: ", x2)
+else: 
+    if d==0:
+        print("el valor:", x1, "es igual a:", x2)
+    else:
+	    if d<0:
+	        print("este problema no tiene solucion en los numeros reales")
